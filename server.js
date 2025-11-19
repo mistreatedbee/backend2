874ca4772fa3150c0c3dfab019ae09dc7ea1aa78
Ashley Mashigo <153994875+mistreatedbee@users.noreply.github.com>
2025-11-19 10:10:03 +0200

    Add 'Accept' to allowedHeaders in CORS configuration

--- server.js
+++ server.js
@@ -85,7 +85,8 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
+    exposedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
